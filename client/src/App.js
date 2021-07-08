ed3b53f42ffe4ecaf289ad7d9a2207c982dab5e7
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-08 21:02:18 +0100

    add hero

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Header from './layout/Header';
-import Footer from './layout/Footer';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
 
 const App = () => {
   return (
     <div className="app">
       <BrowserRouter>
         <Header />
+        <main>
+          <Route exact path="/" component={Home} />
+        </main>
         <Footer />
       </BrowserRouter>
     </div>
---
+++ client/src/components/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='hero__text'>
+        <img
+          src='/images/logo-text.png'
+          alt='Roma'
+          width='320'
+          height='250px'
+        />
+        <h2 className='heading-secondary'>Amor em forma de doce</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
