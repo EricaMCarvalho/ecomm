ddad6b3108e35a9dde99106e84de7830ce543d6e
Erica Melo de Carvalho <ericamcar28@gmail.com>
2021-07-10 03:47:13 +0100

    fix heading padding

--- client/src/components/pages/AboutPage.js
+++ client/src/components/pages/AboutPage.js
@@ -3,24 +3,26 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <>
-      <h1 className='heading-primary'>Sobre nós</h1>
-      <h2 className='heading-secondary'>Conheça a gente</h2>
       <div className='about'>
-        <img
-          src='/images/sobre-nos.png'
-          alt=''
-          width='300'
-          className='about__image'
-        />
-        <div className='about__text'>
-          <p>
-            Duas amigas que decidiram inovar suas vidas na pandemia! Eis o
-            resultado: doces finos e lindos para vocês. Queremos dividir com os
-            nossos clientes o que há de melhor na confeitaria e mostrar que o
-            simples pode se tornar sofisticado quando bem feito. Esperamos que
-            amem... afinal tudo aqui é feito com muito amor!
-          </p>
-          <p className='heading-secondary my-5'>Roberta e Maísa</p>
+        <h1 className='heading-primary'>Sobre nós</h1>
+        <h2 className='heading-secondary'>Conheça a gente</h2>
+        <div className='about-inner'>
+          <img
+            src='/images/sobre-nos.png'
+            alt=''
+            width='300'
+            className='about__image'
+          />
+          <div className='about__text'>
+            <p>
+              Duas amigas que decidiram inovar suas vidas na pandemia! Eis o
+              resultado: doces finos e lindos para vocês. Queremos dividir com
+              os nossos clientes o que há de melhor na confeitaria e mostrar que
+              o simples pode se tornar sofisticado quando bem feito. Esperamos
+              que amem... afinal tudo aqui é feito com muito amor!
+            </p>
+            <p className='heading-secondary my-5'>Roberta e Maísa</p>
+          </div>
         </div>
       </div>
     </>
